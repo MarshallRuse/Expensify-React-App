0993e14d6cde83feb884267c2c4d9b4eadc90bbc
Marshall Ruse <12mr9@queensu.ca>
2019-07-24 15:24:16 -0400

    Removed dummy initialization data

--- src/app.js
+++ src/app.js
@@ -14,39 +14,9 @@ import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
-store.subscribe(() => {
-    console.log(store.getState());
-})
-
-console.log(store.getState());
-
-store.dispatch(addExpense({
-    description: 'Water bill',
-    amount: 500
-}));
-store.dispatch(addExpense({
-    description: 'Food bill',
-    amount: 1000
-}));
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 5000,
-    createdAt: 1000
-}));
-store.dispatch(addExpense({
-    description: 'Vet Bill',
-    amount: 2000,
-    createdAt: -500
-}));
-
-
-const state = store.getState();
-
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 // Provider is a HOC that holds the Redux store.
 // It works with 'connect', used in the ExpenseList component, for instance
